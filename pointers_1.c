04d5e1d30e5b32444dbbb9917d4b3e84cee08e14
Sammie <samaneh.emranian@gmail.com>
2021-03-08 21:39:46 -0600

    Add files via upload

---
+++ pointers_1.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : pointers_1.c
+ Author      : Sammie Omranian
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Pointer's example_1
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int m = 25;
+	char ch = 'g';
+	printf("the memory address of m = %x \n", &m);
+	printf("the memory address of ch = %x \n", &ch);
+	return 0;
+}
