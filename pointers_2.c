c352ac7cfd0170c824569b6366e3e6f7faf3d41c
Sammie <samaneh.emranian@gmail.com>
2021-03-08 22:59:35 -0600

    Update pointers_2.c

--- pointers_2.c
+++ pointers_2.c
@@ -4,7 +4,7 @@
  Author      : Sammie Omranian
  Version     :
  Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Description : declare and initialize a pointer
  ============================================================================
  */
 
