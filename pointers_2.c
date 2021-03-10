00d1cdbd8e27531ee03a27caed7ff8438dfeb59c
Sammie <samaneh.emranian@gmail.com>
2021-03-10 00:16:02 -0600

    Add files via upload

--- pointers_2.c
+++ pointers_2.c
@@ -1,21 +1,34 @@
 /*
- ============================================================================
- Name        : pointers_2.c
- Author      : Sammie Omranian
- Version     :
- Copyright   : Your copyright notice
- Description : declare and initialize a pointer
- ============================================================================
- */
+/* Inputs each number from an input file and writes it
+   rounded to 2 decimal places on a line of an output file.
+*/
+
 
 #include <stdio.h>
 
 int main(void) {
-	int m = 25;
-	int *p;
-	p = &m;
-	printf("The address stores in p is: %x ", p);
-	printf("\n\tThe address of m is: %x ", &m);
+	FILE *inp;		/*pointer to input file*/
+	FILE *outp;		/*pointer to output file*/
+	double item;	/*input number*/
+	int input_status; 	/* status value returned by fscanf */
 
-	return (0);
+/* Prepare files for input or output */
+	inp = fopen("indata.txt", "r");  	//inp is holding the memory location
+	outp = fopen("outdata.txt", "w");
+
+
+/* Input each item, format it, and write it */
+	input_status = fscanf(inp, "%lf", &item);
+
+	while (input_status == 1)
+	{
+		fprintf(outp, "%.2f\n", item);
+		input_status = fscanf(inp, "%lf", &item);
+	}
+
+	/* Close the files */
+	fclose(inp);
+	fclose(outp);
+
+	return 0;
 }
